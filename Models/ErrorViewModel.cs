6e9c891292fa02bdbd5128b03af5e0260ccd3ed1
Rafael-155 <142855150+Rafael-155@users.noreply.github.com>
2024-03-17 16:21:00 -0500

    Se agrega el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ejercicio_github.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
